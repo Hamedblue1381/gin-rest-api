fc957495adf6b31f8b6fac945ad8fcb7197753d6
hamedblue1381 <hamed.balanar@gmail.com>
2023-06-15 20:22:36 +0330

    feat: add gin routes

--- main.go
+++ main.go
@@ -19,5 +19,21 @@ var albums = []album{
 
 func main() {
 	router := gin.Default()
+	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID)
+	router.POST("/albums", postAlbums)
+
 	router.Run("localhost:8080")
+}
+
+func getAlbums(c *gin.Context) {
+
+}
+
+func postAlbums(c *gin.Context) {
+
+}
+
+func getAlbumByID(c *gin.Context) {
+
 }
